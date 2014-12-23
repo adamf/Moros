cf6167e90dec18986922b430b0ad9b41b0d8fb78
Adam Fletcher <adamf@csh.rit.edu>
2014-12-22 17:03:42 -0800

    Clean up edges, prep files for OSHpark fab

---
+++ shield/gerber/rename_and_zip.sh
@@ -0,0 +1,10 @@
+mv moros-F_Cu.gbr moros.GTL
+mv moros-B_Cu.gbr moros.GBL
+mv moros-F_Mask.gbr moros.GTS
+mv moros-B_Mask.gbr moros.GBS
+mv moros-F_SilkS.gbr moros.GTO
+mv moros-B_SilkS.gbr moros.GBO
+mv moros-Edge_Cuts.gbr moros.GKO
+mv moros.drl moros.XLN
+rm moros.zip 
+zip moros.zip moros.G* moros.XLN 
